d5409c04f8ebc2e3bd6c0e844f0337a232b583f5
renyh <18281511768@163.com>
2024-07-24 14:37:18 +0800

    header:   type: study   scope: 代码   id: 无   subject: jvm知识点复习

---
+++ spring-night/src/main/java/com/ral/young/night/jvm/CpuSoaring.java
@@ -0,0 +1,18 @@
+package com.ral.young.night.jvm;
+
+/**
+ * 通过 top 结合 jstack 分析 cpu 飙升的情况
+ *
+ * @author renyunhui
+ * @date 2024-07-24 14:17
+ * @since 1.0.0
+ */
+public class CpuSoaring {
+
+    public static void main(String[] args) {
+        System.out.println("验证 cpu 飙升");
+        for (;;) {
+
+        }
+    }
+}
