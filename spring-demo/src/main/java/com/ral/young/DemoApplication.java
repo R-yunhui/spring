b0528e68ec6ee7d507858c3e6e76ea4b60776f88
renyh <renyunhui@yourangroup.com>
2024-09-05 11:03:44 +0800

    header:   type: study   scope: 代码   id: 无   subject: spring 学习

---
+++ spring-demo/src/main/java/com/ral/young/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.ral.young;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author renyunhui
+ * @description 启动类
+ * @date 2024-09-05 10-50-13
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
